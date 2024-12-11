b672161fdeb931aab48918202b847630255b6ae1
NMurdaugh <nmurdaugh42@gmail.com>
2024-12-11 01:12:44 -0600

    Add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/image", "@vueuse/nuxt"],
   future: {
     compatibilityVersion: 4,
   },
