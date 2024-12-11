4ae3ded4d842ae193904d123075f92db92440e44
NMurdaugh <nmurdaugh42@gmail.com>
2024-12-11 01:40:08 -0600

    Add content and il8n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxt/image", "@vueuse/nuxt"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@nuxt/content",
+    "@nuxtjs/i18n",
+  ],
   future: {
     compatibilityVersion: 4,
   },
