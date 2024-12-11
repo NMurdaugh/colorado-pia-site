438a37665b8dae017b8a6e57d041f36308e7d060
NMurdaugh <nmurdaugh42@gmail.com>
2024-12-11 00:54:38 -0600

    Add NuxtUIPro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui"],
   future: {
     compatibilityVersion: 4,
   },
