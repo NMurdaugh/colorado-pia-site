9e5ca38d284874693c030c4d23150ab7416e8df6
NMurdaugh <nmurdaugh42@gmail.com>
2024-12-11 17:21:45 -0600

    Add layout; Add default header with links and colormode

---
+++ app/components/DefaultHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <UHeader :links>
+    <template #right?>
+      <UColorModeButton />
+    </template>
+  </UHeader>
+</template>
+
+<script setup>
+  const links = [
+    {
+      label: "About Us",
+      to: "/about",
+    },
+    {
+      label: "Services",
+      to: "/services",
+    },
+  ];
+</script>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto max-w-5xl">
+    <DefaultHeader />
+    <main class="p-2 mt-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+  useHead({
+    titleTemplate: "%s - Nicholas Murdaugh",
+  });
+</script>
