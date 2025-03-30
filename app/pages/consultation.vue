aa10ca9914264e8a2695a6945307519961aaefea
NMurdaugh <nmurdaugh42@gmail.com>
2025-03-30 12:37:05 -0500

    Add consultation page

---
+++ app/pages/consultation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ULandingSection
+      v-if="page"
+      :title="page.title"
+      :description="page.description"
+      class="py-16 bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800"
+    >
+      <ContactForm />
+    </ULandingSection>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const route = useRoute();
+  const { data: page } = await useAsyncData("contact", () =>
+    queryContent("/contact").findOne()
+  );
+
+  useHead({
+    title: "Contact",
+  });
+</script>
+
+<style></style>
