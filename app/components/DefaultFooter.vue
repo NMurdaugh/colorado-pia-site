9762ce97673d43fe1f7f953a7950b53dcacdd537
NMurdaugh <nmurdaugh42@gmail.com>
2025-05-21 14:01:02 -0500

    Add simple footer

---
+++ app/components/DefaultFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <UFooter>
+    <template #left>
+      Copyright © {{ new Date().getFullYear() }} Colorado Protection and
+      Investigation Agency. All rights reserved.
+    </template>
+  </UFooter>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,6 +4,7 @@
     <main>
       <slot />
     </main>
+    <DefaultFooter />
   </div>
 </template>
 
